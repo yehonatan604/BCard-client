8079b17b9b88111650d81e366d8b2cc809147d02
Yehonatan Moravia <97249220+yehonatan604@users.noreply.github.com>
2024-05-22 02:32:55 +0300

    fix

--- src/UI/pages/Biz/Biz.page.tsx
+++ src/UI/pages/Biz/Biz.page.tsx
@@ -1,31 +1,36 @@
+//** Dependencies **//
 import { useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useEffect, useState, MouseEvent } from "react";
 import useAPI from "../../../core/hooks/useAPI";
 import { HttpMethods } from "../../../data/enums/HttpMethods.enum";
 import { ICard } from "../../../data/types/ICard";
 import { paths } from "../../../data/constants/paths";
 import Flex from "../../components/shared/Flex/Flex.component";
 import { FlexDirs } from "../../../data/enums/FlexDirs.enum";
-import { Textarea } from "flowbite-react";
 import { FlexTypes } from "../../../data/enums/FlexTypes.enum";
+import Styles from "./Biz.styles";
 
+//** Biz component **//
 const Biz = () => {
+    const {VITE_GOOGLE_MAPS_API_KEY:KEY} = import.meta.env;    
+    
+  //** State **//
   const [card, setCard] = useState<ICard | null>(null);
+
+  //** Hooks **//
   const id = useLocation().pathname.split("/")[2];
   const { sendApiRequest } = useAPI();
 
-  const callNumber = (e: React.MouseEvent<HTMLParagraphElement>) => {
-    window.open(`tel:${e.currentTarget.textContent}`);
+  //** Functions **//
+  const open = (e: MouseEvent<HTMLParagraphElement>) => {
+    const id = e.currentTarget.id;
+    const target = e.currentTarget.textContent;
+    id === "web"
+      ? window.open(`${target}`, "_blank")
+      : window.open(`${id}:${target}`);
   };
 
-  const sendEmail = (e: React.MouseEvent<HTMLParagraphElement>) => {
-    window.open(`mailto:${e.currentTarget.textContent}`);
-  };
-
-  const openWeb = (e: React.MouseEvent<HTMLParagraphElement>) => {
-    window.open(`${e.currentTarget.textContent}`);
-  };
-
+  //** Effects **//
   useEffect(() => {
     (async () => {
       try {
@@ -40,56 +45,57 @@ const Biz = () => {
     })();
   }, [id, sendApiRequest]);
 
+  //** JSX **//
+  if (!card)
+    return <h1 className={Styles.warning}>No card with that Id was found!</h1>;
+
   return (
     <Flex
       dir={FlexDirs.Column}
       items={FlexTypes.Start}
-      className="m-auto w-[60vw]"
+      className={Styles.container}
     >
-      <Flex className="my-2 w-full">
-        <h1 className="overflow-hidden text-3xl">{card?.title}</h1>
+      <Flex className={Styles.titleContainer}>
+        <h1 className={Styles.title}>{card?.title}</h1>
       </Flex>
-      <Flex className="my-4 w-full">
-        <h3 className="text-xl">{card?.subtitle}</h3>
+      <Flex className={Styles.subtitleContainer}>
+        <h3 className={Styles.subtitle}>{card?.subtitle}</h3>
       </Flex>
-      <Flex justify={FlexTypes.Start} className="m-auto my-5 w-1/2 bg-slate-300 border border-violet-300 shadow-xl shadow-slate-800 rounded-xl">
-        <h4 className="h-[15vh] p-3 cursor-default">
-          {card?.description}
-        </h4>
+      <Flex justify={FlexTypes.Start} className={Styles.descriptionContainer}>
+        <h4 className={Styles.description}>{card?.description}</h4>
       </Flex>
       <Flex
         dir={FlexDirs.Column}
         items={FlexTypes.Start}
-        className="m-auto my-5 w-1/2"
+        className={Styles.linksContainer}
       >
-        <Flex className="gap-2">
+        <Flex className={Styles.linksContainerItem}>
           <p>phone:</p>
-          <p
-            className="cursor-pointer hover:text-blue-500"
-            onClick={callNumber}
-          >
+          <p id="tel" className={Styles.link} onClick={open}>
             {card?.phone}
           </p>
         </Flex>
-        <Flex className="gap-2">
+        <Flex className={Styles.linksContainerItem}>
           <p>email:</p>
-          <p className="cursor-pointer hover:text-blue-500" onClick={sendEmail}>
+          <p id="mailto" className={Styles.link} onClick={open}>
             {card?.email}
           </p>
         </Flex>
-        {card?.web && <Flex className="gap-2">
-          <p>web:</p>
-          <p className="cursor-pointer hover:text-blue-500" onClick={openWeb}>
-            {card?.web}
-          </p>
-        </Flex>}
+        {card?.web && (
+          <Flex className={Styles.linksContainerItem}>
+            <p>web:</p>
+            <p id="web" className={Styles.link} onClick={open}>
+              {card?.web}
+            </p>
+          </Flex>
+        )}
       </Flex>
-      <Flex className="mb-7 h-[60vh] w-full shadow-md shadow-slate-900">
+      <Flex className={Styles.mapContainer}>
         <iframe
-          className="h-full w-full rounded-lg "
+          className={Styles.map}
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
-          src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyA0ONZpJibVTK2YEDO8NSAuZW9qQmWejwk
+          src={`https://www.google.com/maps/embed/v1/place?key=${KEY}
     &q=${card?.address.street} ${card?.address.city} ${card?.address.country}`}
         ></iframe>
       </Flex>
---
+++ src/UI/pages/Biz/Biz.styles.ts
@@ -0,0 +1,46 @@
+import { normalized } from "../../../core/helpers/Style.Helper";
+import { Style } from "../../../data/models/Style.model";
+
+const styles: Record<string, Style> = {
+    container: new Style({
+        all: `m-auto w-[60vw]`,
+    }),
+    titleContainer: new Style({
+        all: `my-2 w-full`,
+    }),
+    title: new Style({
+        all: `overflow-hidden text-3xl`,
+    }),
+    subtitleContainer: new Style({
+        all: `my-4 w-full`,
+    }),
+    subtitle: new Style({
+        all: `text-xl`,
+    }),
+    descriptionContainer: new Style({
+        all: `m-auto my-5 w-1/2 bg-slate-100 border border-violet-300 shadow-xl shadow-slate-500 rounded-xl`,
+    }),
+    description: new Style({
+        all: `h-[15vh] p-3 cursor-default`,
+    }),
+    linksContainer: new Style({
+        all: `m-auto my-5 w-1/2`,
+    }),
+    linksContainerItem: new Style({
+        all: `gap-2`,
+    }),
+    link: new Style({
+        all: `cursor-pointer hover:text-blue-500`,
+    }),
+    mapContainer: new Style({
+        all: `mb-7 h-[0vh] w-full shadow-md shadow-slate-900`,
+    }),
+    map: new Style({
+        all: `h-full w-full rounded-lg`,
+    }),
+    warning: new Style({
+        all: `text-3xl text-center m-4`,
+    }),
+}
+
+export default normalized(styles);
